dc1599c8c26a98e6242bc1ea2001b51256843e4d
pouria azad <109802957+pouria-azad@users.noreply.github.com>
2024-12-26 23:35:39 +0330

    Update database.php

--- config/database.php
+++ config/database.php
@@ -85,11 +85,11 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DB_URL'),
-            'host' => env('DB_HOST', 'modest-robinson-njopxmiln-db'),
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'laravel'),
             'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', 'mFcQx4RPtzJoqMUHaEBcJRIX'),
+            'password' => env('DB_PASSWORD', 'ybueGln2HZg1E0Hhjgv4O6Bj'),
             'charset' => env('DB_CHARSET', 'utf8'),
             'prefix' => '',
             'prefix_indexes' => true,
