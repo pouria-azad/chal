b5ffa87626cde72790c45fdd0d6bb465c61c7e96
pouria azad <109802957+pouria-azad@users.noreply.github.com>
2024-12-26 23:21:12 +0330

    Update database.php

--- config/database.php
+++ config/database.php
@@ -85,7 +85,7 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DB_URL'),
-            'host' => env('DB_HOST', 'postgresql://root:ybueGln2HZg1E0Hhjgv4O6Bj@modest-robinson-njopxmiln-db:5432/postgres'),
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'laravel'),
             'username' => env('DB_USERNAME', 'root'),
